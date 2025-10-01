82d3c0366d7a27b164ab7ccbe698ae395816a271
Yorch MacMini <yorchmacmini@Yorchs-Mac-mini.local>
2025-10-01 10:49:10 -0500

    Montar carpeta static

--- main.py
+++ main.py
@@ -67,9 +67,11 @@ app = FastAPI(title="API - Cotizador 48 Voltios")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 # ---------------------------
 # Precios (constantes)
